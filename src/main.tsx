c02c13af5b7f318f4df551fe3e8a3563709573cb
s2robin <karolrobinruiz.su@gmail.com>
2026-04-28 18:13:07 -0300

    Add build id to bust caches

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Helps bust caches in production deployments.
+document.documentElement.dataset.build = '2026-04-28.1';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
